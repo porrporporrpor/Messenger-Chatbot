f02921d2c5274a5cf275daac9efbfb93613c2b42
Benjawan Simpattanawong <benjawan.s@teohong.com>
2021-02-25 23:54:13 +0700

    Edit Message struct and Add Attachment, Payload struct

--- model/messenger.go
+++ model/messenger.go
@@ -1,21 +1,68 @@
 package model
 
+/**
+Media in Message struct can be
+- text string : for normal text message and emoji
+- attachments interface : for audio, video and image (sticker, gif also seen as image)
+*/
+
+/**
+audio
+type : audio
+payload : url
+
+video
+type : video
+payload : url
+
+
+gif
+type : image
+payload : url
+
+image
+type : image
+payload : url
+
+sticker
+type : image
+payload : sticker_id , url
+*/
+
+type Payload struct {
+	StickerID *string `json:"sticker_id,omitempty"`
+	URL       string  `json:"url"`
+}
+
+type Attachment struct {
+	Type    string  `json:"type"`
+	Payload Payload `json:"payload"`
+}
+
 type Sender struct {
 	ID string `json:"id"`
 }
 type Recipient struct {
 	ID string `json:"id"`
 }
+
 type Message struct {
+	MID         string        `json:"mid,omitempty"`
+	Text        string        `json:"text,omitempty"`
+	Attachments *[]Attachment `json:"attachments,omitempty"`
+}
+
+type Messaging struct {
 	Sender    Sender    `json:"sender"`
 	Recipient Recipient `json:"recipient"`
 	Timestamp float64   `json:"timestamp"`
+	Message   Message
 }
 
 type Entry struct {
-	ID        string  `json:"id"`
-	Time      float64 `json:"time"`
-	Messaging []Message
+	ID        string         `json:"id"`
+	Time      float64        `json:"time"`
+	Messaging *[]Messaging   `json:"messaging"`
 }
 
 type MessengerRequestBody struct {

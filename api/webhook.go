5817d3ea86ab71b29030d46ae23f95ea6de19694
Benjawan Simpattanawong <benjawan.s@teohong.com>
2021-03-01 22:52:28 +0700

    Update webhook for use Receipt and Button template

--- api/webhook.go
+++ api/webhook.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/openlyinc/pointy"
 	"github.com/rs/zerolog/log"
 	"gitlab.com/pplayground/messenger-chatbot/backend/model"
 	"gitlab.com/pplayground/messenger-chatbot/backend/service"
@@ -34,7 +35,55 @@ func (api WebhookAPI) ReceiveFromWebhook(ctx *gin.Context) {
 		if messaging.Message.QuickReply != nil {
 			switch messaging.Message.QuickReply.Payload {
 			case "SHOP_NOW":
-				err := api.MessengerService.CreateShopNowTemplate(messaging.Sender.ID)
+				err := api.MessengerService.CreateGenericTemplate(messaging.Sender.ID, model.TemplateGenericPayload{
+					TemplateType: "generic",
+					Elements: []model.GenericElement{
+						{
+							Title:    "Product Name 1",
+							ImageUrl: "https://picsum.photos/200",
+							Subtitle: "Description",
+							DefaultAction: model.CallToAction{
+								Type:               "web_url",
+								URL:                pointy.String("https://picsum.photos/200"),
+								WebviewHeightRatio: pointy.String("tall"),
+							},
+							Buttons: []model.CallToAction{
+								{
+									Type:    "postback",
+									Title:   pointy.String("Shop Now !"),
+									Payload: pointy.String("VIEW_PRODUCT_1"),
+								},
+								{
+									Type:  "web_url",
+									Title: pointy.String("Instagram"),
+									URL:   pointy.String("https://instagram.com"),
+								},
+							},
+						},
+						{
+							Title:    "Product Name 2",
+							ImageUrl: "https://picsum.photos/200",
+							Subtitle: "Description",
+							DefaultAction: model.CallToAction{
+								Type:               "web_url",
+								URL:                pointy.String("https://picsum.photos/200"),
+								WebviewHeightRatio: pointy.String("tall"),
+							},
+							Buttons: []model.CallToAction{
+								{
+									Type:    "postback",
+									Title:   pointy.String("Shop Now !"),
+									Payload: pointy.String("VIEW_PRODUCT_2"),
+								},
+								{
+									Type:  "web_url",
+									Title: pointy.String("Instagram"),
+									URL:   pointy.String("https://instagram.com"),
+								},
+							},
+						},
+					},
+				})
 				if err != nil {
 					log.Error().
 						Str("type", model.LogTypeAPI).
@@ -45,10 +94,54 @@ func (api WebhookAPI) ReceiveFromWebhook(ctx *gin.Context) {
 						Payload: "cannot create shop now template menu"})
 				}
 			case "MY_ORDER":
-				log.Debug().
-					Str("type", model.LogTypeAPI).
-					Str("status", model.LogStatusData).
-					Msg("my order is not implement")
+				err := api.MessengerService.CreateReceiptTemplate(messaging.Sender.ID, model.TemplateReceiptPayload{
+					TemplateType:  "receipt",
+					RecipientName: "Benjawan S.",
+					OrderName:     "123456",
+					Currency:      "THB",
+					PaymentMethod: "Cash",
+					OrderUrl:      "http://petersapparel.parseapp.com/order?order_id=123456",
+					Timestamp:     "1428444852",
+					Summary: model.ReceiptSummary{
+						Subtotal:     750.00,
+						ShippingCost: 40.95,
+						TotalTax:     60.19,
+						TotalCost:    460.14,
+					},
+					Adjustments: &[]model.ReceiptAdjustments{
+						{
+							Name:   "Discount",
+							Amount: 100,
+						},
+					},
+					Elements: []model.ReceiptElement{
+						{
+							Title:    "Classic White T-Shirt",
+							Subtitle: "100% Soft and Luxurious Cotton",
+							Quantity: 2,
+							Price:    500,
+							Currency: "THB",
+							ImageUrl: "https://picsum.photos/200",
+						},
+						{
+							Title:    "Classic Pink T-Shirt",
+							Subtitle: "100% Soft and Luxurious Cotton",
+							Quantity: 1,
+							Price:    250,
+							Currency: "THB",
+							ImageUrl: "https://picsum.photos/200",
+						},
+					},
+				})
+				if err != nil {
+					log.Error().
+						Str("type", model.LogTypeAPI).
+						Str("status", model.LogStatusFailed).
+						Msg("operation CreateReceiptTemplate service failed")
+					ctx.JSON(http.StatusInternalServerError, model.ResponsePayload{
+						Status:  model.LogStatusFailed,
+						Payload: "cannot create receipt template of my order menu"})
+				}
 			}
 		}
 	}
@@ -96,7 +189,55 @@ func (api WebhookAPI) ReceiveFromWebhook(ctx *gin.Context) {
 					Payload: "cannot create persistent menu"})
 			}
 		case "SHOP_NOW":
-			err := api.MessengerService.CreateShopNowTemplate(messaging.Sender.ID)
+			err := api.MessengerService.CreateGenericTemplate(messaging.Sender.ID, model.TemplateGenericPayload{
+				TemplateType: "generic",
+				Elements: []model.GenericElement{
+					{
+						Title:    "Product Name 1",
+						ImageUrl: "https://picsum.photos/200",
+						Subtitle: "Description",
+						DefaultAction: model.CallToAction{
+							Type:               "web_url",
+							URL:                pointy.String("https://picsum.photos/200"),
+							WebviewHeightRatio: pointy.String("tall"),
+						},
+						Buttons: []model.CallToAction{
+							{
+								Type:    "postback",
+								Title:   pointy.String("Shop Now !"),
+								Payload: pointy.String("VIEW_PRODUCT_1"),
+							},
+							{
+								Type:  "web_url",
+								Title: pointy.String("Instagram"),
+								URL:   pointy.String("https://instagram.com"),
+							},
+						},
+					},
+					{
+						Title:    "Product Name 2",
+						ImageUrl: "https://picsum.photos/200",
+						Subtitle: "Description",
+						DefaultAction: model.CallToAction{
+							Type:               "web_url",
+							URL:                pointy.String("https://picsum.photos/200"),
+							WebviewHeightRatio: pointy.String("tall"),
+						},
+						Buttons: []model.CallToAction{
+							{
+								Type:    "postback",
+								Title:   pointy.String("Shop Now !"),
+								Payload: pointy.String("VIEW_PRODUCT_2"),
+							},
+							{
+								Type:  "web_url",
+								Title: pointy.String("Instagram"),
+								URL:   pointy.String("https://instagram.com"),
+							},
+						},
+					},
+				},
+			})
 			if err != nil {
 				log.Error().
 					Str("type", model.LogTypeAPI).
@@ -107,16 +248,136 @@ func (api WebhookAPI) ReceiveFromWebhook(ctx *gin.Context) {
 					Payload: "cannot create shop now template menu"})
 			}
 		case "MY_ORDER":
-			log.Debug().
-				Str("type", model.LogTypeAPI).
-				Str("status", model.LogStatusData).
-				Msg("my order is not implement")
+			err := api.MessengerService.CreateReceiptTemplate(messaging.Sender.ID, model.TemplateReceiptPayload{
+				TemplateType:  "receipt",
+				RecipientName: "Benjawan S.",
+				OrderName:     "123456",
+				Currency:      "THB",
+				PaymentMethod: "Cash",
+				OrderUrl:      "http://petersapparel.parseapp.com/order?order_id=123456",
+				Timestamp:     "1428444852",
+				Summary: model.ReceiptSummary{
+					Subtotal:     750.00,
+					ShippingCost: 40.95,
+					TotalTax:     60.19,
+					TotalCost:    460.14,
+				},
+				Adjustments: &[]model.ReceiptAdjustments{
+					{
+						Name:   "Discount",
+						Amount: 100,
+					},
+				},
+				Elements: []model.ReceiptElement{
+					{
+						Title:    "Classic White T-Shirt",
+						Subtitle: "100% Soft and Luxurious Cotton",
+						Quantity: 2,
+						Price:    500,
+						Currency: "THB",
+						ImageUrl: "https://picsum.photos/200",
+					},
+					{
+						Title:    "Classic Pink T-Shirt",
+						Subtitle: "100% Soft and Luxurious Cotton",
+						Quantity: 1,
+						Price:    250,
+						Currency: "THB",
+						ImageUrl: "https://picsum.photos/200",
+					},
+				},
+			})
+			if err != nil {
+				log.Error().
+					Str("type", model.LogTypeAPI).
+					Str("status", model.LogStatusFailed).
+					Msg("operation CreateReceiptTemplate service failed")
+				ctx.JSON(http.StatusInternalServerError, model.ResponsePayload{
+					Status:  model.LogStatusFailed,
+					Payload: "cannot create receipt template of my order menu"})
+			}
 		default:
 			if strings.Contains(messaging.Postback.Payload, "VIEW_PRODUCT") {
-				log.Debug().
-					Str("type", model.LogTypeAPI).
-					Str("status", model.LogStatusData).
-					Msg(fmt.Sprintf("postback payload : %v", messaging.Postback.Payload))
+				err := api.MessengerService.CreateButtonTemplate(messaging.Sender.ID, model.TemplateButtonPayload{
+					TemplateType: "button",
+					Text:         "กรุณาระบุจำนวนที่ต้องการสั่งซื้อ (เพิ่มเติม: สั่งมากกว่า 3 ชิ้นโปรดติดต่อแอดมิน)",
+					Button: []model.CallToAction{
+						{
+							Type:    "postback",
+							Title:   pointy.String("1"),
+							Payload: pointy.String("QTY_1"),
+						},
+						{
+							Type:    "postback",
+							Title:   pointy.String("2"),
+							Payload: pointy.String("QTY_2"),
+						},
+						{
+							Type:    "postback",
+							Title:   pointy.String("3"),
+							Payload: pointy.String("QTY_3"),
+						},
+					},
+				})
+				if err != nil {
+					log.Error().
+						Str("type", model.LogTypeAPI).
+						Str("status", model.LogStatusFailed).
+						Msg("operation CreateButtonTemplate service failed")
+					ctx.JSON(http.StatusInternalServerError, model.ResponsePayload{
+						Status:  model.LogStatusFailed,
+						Payload: "cannot create button template"})
+				}
+			}
+			if strings.Contains(messaging.Postback.Payload, "QTY") {
+				err := api.MessengerService.CreateReceiptTemplate(messaging.Sender.ID, model.TemplateReceiptPayload{
+					TemplateType:  "receipt",
+					RecipientName: "Benjawan S.",
+					OrderName:     "123456",
+					Currency:      "THB",
+					PaymentMethod: "Cash",
+					OrderUrl:      "http://petersapparel.parseapp.com/order?order_id=123456",
+					Timestamp:     "1428444852",
+					Summary: model.ReceiptSummary{
+						Subtotal:     750.00,
+						ShippingCost: 40.95,
+						TotalTax:     60.19,
+						TotalCost:    460.14,
+					},
+					Adjustments: &[]model.ReceiptAdjustments{
+						{
+							Name:   "Discount",
+							Amount: 100,
+						},
+					},
+					Elements: []model.ReceiptElement{
+						{
+							Title:    "Classic White T-Shirt",
+							Subtitle: "100% Soft and Luxurious Cotton",
+							Quantity: 2,
+							Price:    500,
+							Currency: "THB",
+							ImageUrl: "https://picsum.photos/200",
+						},
+						{
+							Title:    "Classic Pink T-Shirt",
+							Subtitle: "100% Soft and Luxurious Cotton",
+							Quantity: 1,
+							Price:    250,
+							Currency: "THB",
+							ImageUrl: "https://picsum.photos/200",
+						},
+					},
+				})
+				if err != nil {
+					log.Error().
+						Str("type", model.LogTypeAPI).
+						Str("status", model.LogStatusFailed).
+						Msg("operation CreateReceiptTemplate service failed")
+					ctx.JSON(http.StatusInternalServerError, model.ResponsePayload{
+						Status:  model.LogStatusFailed,
+						Payload: "cannot create receipt template of my order menu"})
+				}
 			}
 		}
 	}
